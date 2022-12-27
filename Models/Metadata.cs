2a98f96d8bcd50036be7a83da36a20d76fdc8bee
felipetriana86 <gatosilvestre6910@gmail.com>
2022-12-27 18:05:41 -0500

    first commit

---
+++ Models/Metadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuildingManagementProject.Models
+{
+    public class Metadata
+    {
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [Display(Name = "Email Address")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+        ErrorMessage = "Email is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        //[Range(0.01, 100.00,
+        //ErrorMessage = "Price must be between 0.01 and 100.00")]
+        //public decimal Price { get; set; }
+    }
+}
